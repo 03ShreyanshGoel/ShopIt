91804686f1b6334a53fff37cb0d4c92b6cf38f4b
shreyansh goel <03shreyanshgoel@gmail.com>
2025-01-14 19:02:46 +0530

    replaced bcrypt with bcryptjs

--- backened/services/authServices.js
+++ backened/services/authServices.js
@@ -1,14 +1,17 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");  // Use bcryptjs instead of bcrypt
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config/envConfig");
+
 module.exports.hashedPassword = async (password) => {
-  salt = await bcrypt.genSalt(10);
+  const salt = await bcrypt.genSalt(10);  // bcryptjs works the same way as bcrypt
   const hashed = await bcrypt.hash(password, salt);
   return hashed;
 };
+
 module.exports.comparePassword = async (password, dbPassword) => {
-  return await bcrypt.compare(password, dbPassword);
+  return await bcrypt.compare(password, dbPassword); 
 };
+
 module.exports.createToken = (user) => {
   return jwt.sign(user, JWT_SECRET, {
     expiresIn: "7d",
